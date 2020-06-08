b17f3ac86c80d2f8567257f7b425241705948778
Felipe Marchant F. Soares <felipemarchant.dev@gmail.com>
2020-06-08 12:21:41 -0300

    Autenticação

--- backend/src/app/middlewares/auth.js
+++ backend/src/app/middlewares/auth.js
@@ -1,4 +1,16 @@
-export default (req, res, next) => {
+import jwt from 'jsonwebtoken';
+import { secret } from '../../config/auth';
+import { promisify } from 'util';
+
+export default async (req, res, next) => {
     const auth = req.headers.authorization;
+    if (!auth) return res.status(401).json({ error: 'Token not provided' });
+    const [, token] = auth.split(' ');
+    try {
+        const decoded = await promisify(jwt.verify)(token, secret);
+        req.userId = decoded.id;
+    } catch(err) {
+        return res.status(401).json({ error: 'Token invavlid' });
+    }
     return next();
 }
