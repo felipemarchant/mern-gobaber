cf58cfbe44148f8fb76277a08bcd39a5cb233e73
Felipe Marchant F. Soares <felipemarchant.dev@gmail.com>
2020-06-08 11:48:24 -0300

    Adição rota store

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,7 +3,8 @@ module.exports = {
     host: 'localhost',
     username: 'postgres',
     password: 'docker',
-    database: 'gobaber',
+    database: 'postgres',
+    port: 5444,
     define: {
         timestamps:  true,
         underscored: true,
