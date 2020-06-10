21b4730cda3842278d499f7d3e575c28726570a3
Felipe Marchant F. Soares <felipemarchant.dev@gmail.com>
2020-06-10 01:02:05 -0300

    Adiaçao de filas no projeto

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -3,7 +3,8 @@ import * as Yup from 'yup';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
-import Mail from '../../lib/Mail';
+import Queue from '../../lib/Queue';
+import CancellationMail from '../jobs/CancellationMail';
 
 import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt-BR';
@@ -82,21 +83,7 @@ class AppointmentController {
             return res.status(401).json({ error: 'You can only cancel appointments 2 hours in advance.' });
         appointment.canceled_at = new Date();
         await appointment.save();
-        await Mail.sendMail({
-            to: `${appointment.provider.name} <${appointment.provider.email}>`,
-            subject: 'Agendamento cancelado!',
-            text: 'Você tem um novo de cancelamento',
-            template: 'cancellation', 
-            context: {
-                provider: appointment.provider.name,
-                user: appointment.user.name,
-                date: format(
-                    appointment.date,
-                    "'dia' dd 'de' MMMM', às' H:mm'h'",
-                    { locale: pt }
-                )
-            }
-        });
+        await Queue.add(CancellationMail.key, { appointment });
         return res.json(appointment);
     }
 }
